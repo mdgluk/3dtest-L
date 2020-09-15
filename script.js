75a89a40ade29d4faa85bb1c255b7a2b43593921
mdgluk <39958032+mdgluk@users.noreply.github.com>
2020-09-15 13:48:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(5, 90, 1, 1000);
+camera.position.set(0, 0, 20);
+var renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
+var canvas = renderer.domElement;
+document.body.appendChild(canvas);
+
+var light = new THREE.DirectionalLight(0xffffff, 0.5);
+light.position.setScalar(10);
+scene.add(light);
+scene.add(new THREE.AmbientLight(0xffffff, 0.5));
+
+var base = new THREE.Group();
+scene.add(base);
+var objectLoader = new THREE.ObjectLoader();
+objectLoader.load( "laura.json", function ( obj ) {
+  obj.rotation.y = -Math.PI * 0;
+  obj.rotation.z = 5;
+  base.add( obj );
+} );
+
+var plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), -10);
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+var pointOfIntersection = new THREE.Vector3();
+canvas.addEventListener("mousemove", onMouseMove, false);
+
+function onMouseMove(event){
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  raycaster.setFromCamera(mouse, camera);
+  raycaster.ray.intersectPlane(plane, pointOfIntersection);
+  base.lookAt(pointOfIntersection);
+}
+
+renderer.setAnimationLoop(() => {
+  if (resize(renderer)) {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+  renderer.render(scene, camera);
+});
+
+function resize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
